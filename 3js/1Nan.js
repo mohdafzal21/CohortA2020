bc5858c259a3eb0ebef71d01b87e9158c747f7e4
Mohammed Afzal <mohammedafzal94@gmail.com>
2020-01-15 17:43:07 +0530

    added intro to js

---
+++ 3js/1Nan.js
@@ -0,0 +1,25 @@
+var myage = Number('0o36');
+
+var nextAge = Number('39'); // 39
+
+var CatAge = Number('n/a');
+
+myage = 'my age text';
+
+// myage - CatAge 
+// 2 - 'n/a'
+
+CatAge === CatAge;
+
+isNaN(myage); 
+
+isNaN(CatAge); //true
+
+isNaN('some randonn age ');
+
+Number.isNan(CatAge);
+
+Number.isNaN('some random text is not a number');
+
+// NAN : Invalid Number
+// dont : undefined
